1c3faaeb1232d128a75193c56e4247476e96768e
kiomnd2 <kiomnd2@naver.com>
2021-03-18 11:00:28 +0900

    first commit

---
+++ src/test/java/com/rabbitmq/tutorial/rabbitmqtest/RabbitmqTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.tutorial.rabbitmqtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
